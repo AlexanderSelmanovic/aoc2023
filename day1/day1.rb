e0fcdf8d9b3f478c9192582ee8790ad5760519a9
Alexander Selmanovic <alexander.selmanovic@omegapoint.se>
2023-12-01 16:20:39 +0100

    Added execution time
    The logic runs at approximately 0,0039 seconds. 10'000 runs made

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,31 +1,45 @@
 path = "day1\\input1.txt"
-result = 0
+total_time = 0
 
-File.open(path, "r") do |file|
-    file.each_line do |line|
-        first_digit = "-1"
-        last_digit = "-1"
+10000.times do
+    run_time = 0
+    start_time = Time.now
 
-        line.each_char do |char|
-            if char.match?(/\d/)
-                if first_digit == "-1"
-                    first_digit = char
-                else
-                    last_digit = char
+    result = 0
+
+    File.open(path, "r") do |file|
+        file.each_line do |line|
+            first_digit = "-1"
+            last_digit = "-1"
+
+            line.each_char do |char|
+                if char.match?(/\d/)
+                    if first_digit == "-1"
+                        first_digit = char
+                    else
+                        last_digit = char
+                    end
                 end
             end
-        end
 
-        char_values = ""
+            char_values = ""
 
-        if last_digit == "-1"
-            char_values = first_digit + first_digit
-            result += char_values.to_i
-        else
-            char_values = first_digit + last_digit
-            result += char_values.to_i
+            if last_digit == "-1"
+                char_values = first_digit + first_digit
+                result += char_values.to_i
+            else
+                char_values = first_digit + last_digit
+                result += char_values.to_i
+            end
         end
     end
+
+    end_time = Time.now
+    run_time = end_time - start_time
+    total_time += run_time
 end
 
-puts result
+average_time = total_time / 10000
+
+puts "Average time per run"
+puts average_time
